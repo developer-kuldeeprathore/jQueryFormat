bc54c827ed48022d8b539b02c0afcf1e50566b80
Kuldeep Rathore <developer.kuldeeprathore@gmail.com>
2020-08-31 17:02:26 +0530

    Add files via upload

---
+++ SortedFile.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="SortedFile.cs" company="Tetraskelion Softwares Pvt. Ltd.">
+// Copyright (c) Tetraskelion Softwares Pvt. Ltd. All rights reserved.
+// </copyright>
+
+namespace TravelMint.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Sorted File
+    /// </summary>
+    internal struct SortedFile
+    {
+        /// <summary>
+        /// Gets or sets the file order.
+        /// </summary>
+        /// <value>
+        /// The file order.
+        /// </value>
+        public int FileOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
+        /// <value>
+        /// The name of the file.
+        /// </value>
+        public string FileName { get; set; }
+    }
+}
